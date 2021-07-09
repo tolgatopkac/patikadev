38700cb0afa318f20f59ed8e1cd34ef02852e460
Tolga Topkaç <tolgatopkac09@gmail.com>
2021-07-09 05:25:10 +0300

    update repo

---
+++ patikadev-node.js/03-NodeBasics/npm_module/index.js
@@ -0,0 +1,6 @@
+// Request - Response Döngüsü
+// --- adres çubuğuna www.google.com yazarak uzaktaki google sunucusuna bir request(istek) gönderirsin.
+// --content-length ??
+// remote address
+// GET : sunucu tarafından bilgi istendiğinde kullanılan method
+// POST : sunucu tarafına bilgi gönderildiğinde örneğin bir form doldurulduğunda bu da post yöntemi ile yapılır
